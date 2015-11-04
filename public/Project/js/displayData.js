e23efa77e15b24084e35d3366f0e03ba900ad5d4
Bhavna Patel <bhavna.patel333@gmail.com>
2015-11-04 12:20:19 -0500

    Added delete section

--- public/Project/js/displayData.js
+++ public/Project/js/displayData.js
@@ -1,16 +1,44 @@
 $(document).ready(function(){
+	var temp;
+	$.ajax({
+		url:"/products",
+		method:"GET",
+		success:function(data){
+			for(i=0; i<data.length; i++)
+			{
+				temp = data[i].id;
+				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data[i].itemName+'</h3><p>'+data[i].itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data[i].itemPrice+'</button><a href="#"><span id='+data[i].id+' class="glyphicon glyphicon-remove-sign"></span></a></div></div>');
+			}
+		}
+	});
 	$(".btn").on("click", function(e){
 		var iname = $("input[name=itemName]").val();
 		var idescription = $("input[name=itemDescription]").val();
 		var iprice = $("input[name=itemPrice]").val();
 		$.ajax({
-			url:"/users",
+			url:"/products",
 			method:"POST",
 			data:{itemName:iname, itemDescription:idescription, itemPrice:iprice},
 			success:function(data){
-				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data.itemName+'</h3><p>'+data.itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data.itemPrice+'</button><span class="glyphicon glyphicon-remove-sign"></span></div></div>');
+				temp++;
+				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data.itemName+'</h3><p>'+data.itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data.itemPrice+'</button><a href="#"><span id='+temp+' class="glyphicon glyphicon-remove-sign"></span></a></div></div>');
 			}
 		});
 		e.preventDefault();
+	});
+	$("#d1").on("click", function(e){
+		var a = e.target;	
+		if(e.target.nodeName === "SPAN")
+		{
+			var x = a.id;
+			$.ajax({
+				url:"/products/"+x,
+				method:"DELETE",
+				success:function(data){
+					alert("successfully deleted");
+				}
+			});
+			$("#d1").empty();
+		}
 	});
 });
