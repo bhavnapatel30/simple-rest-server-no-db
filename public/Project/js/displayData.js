cd4e8a2f52fb1f89cea3c752b5b0bc12230612bb
Bhavna Patel <bhavna.patel333@gmail.com>
2015-11-05 11:27:47 -0500

    Made responsive page

--- public/Project/js/displayData.js
+++ public/Project/js/displayData.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	var temp=0;;
 	$.ajax({
 		url:"/products",
 		method:"GET",
 		success:function(data){
 			for(i=0; i<data.length; i++)
 			{
-				temp = data[i].id;
 				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data[i].itemName+'</h3><p>'+data[i].itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data[i].itemPrice+'</button><a href="#"><span id='+data[i].id+' class="glyphicon glyphicon-remove-sign"></span></a></div></div>');
 			}
 		}
@@ -20,10 +18,12 @@ $(document).ready(function(){
 			method:"POST",
 			data:{itemName:iname, itemDescription:idescription, itemPrice:iprice},
 			success:function(data){
-				temp++;
-				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data.itemName+'</h3><p>'+data.itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data.itemPrice+'</button><a href="#"><span id='+temp+' class="glyphicon glyphicon-remove-sign"></span></a></div></div>');
+				$("input[name=itemName]").val("");
+				$("input[name=itemDescription]").val("");
+				$("input[name=itemPrice]").val("");
+				$("#d1").append('<div class="well row"><div class="col-md-8"><h3>'+data.itemName+'</h3><p>'+data.itemDescription+'</p></div><div class="col-md-4"><button class="btn btn-primary">$'+data.itemPrice+'</button><a href="#"><span id='+data.id+' class="glyphicon glyphicon-remove-sign"></span></a></div></div>');
 			}
-		});
+		});//temp, form remove, design
 		e.preventDefault();
 	});
 	$("#d1").on("click", function(e){
