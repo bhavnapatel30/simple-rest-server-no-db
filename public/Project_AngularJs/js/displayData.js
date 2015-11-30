a7f24cdef4d945eee17f93b004c7f56a4a8bc686
Bhavna Patel <bhavna.patel333@gmail.com>
2015-11-30 16:30:25 -0500

    Made changes in displayData.js and main.html

--- public/Project_AngularJs/js/displayData.js
+++ public/Project_AngularJs/js/displayData.js
@@ -1,4 +1,69 @@
 var app = angular.module('myApp', []);
+
+app.factory('itemFactory', function($http){
+	var items;
+	var factory = {};
+	factory.getItems = function(){
+		$http.get("/products").success(function(response){
+			items = response.data;
+		});
+		return items;
+	};
+	return factory;
+});
+			
+app.controller('itemsCtrl', function ($scope, itemFactory){
+	//$scope.items=[];
+	$scope.items = itemFactory.getItems();
+	console.log($scope.items);					
+});
+
+		
+/*app.controller('itemsCtrl', function($scope, $http){
+	$http.get("/products").success(function(response){
+		$scope.items = response;
+	});
+});
+		
+app.controller('postItemCtrl', function($scope,$http){
+	$scope.newItem = {};
+	$scope.showme = false;
+			
+	$scope.display = function(){
+		$scope.newItem  = {
+			itemName: $scope.item_name,
+			itemDescription: $scope.item_description,
+			itemPrice: $scope.item_price
+		};
+
+		$http({
+			method:'POST',
+			url:'/products',
+			data:$scope.newItem
+		}).success(function(response){
+			$scope.newItem = response;
+
+			$scope.item_name="";
+			$scope.item_description="";
+			$scope.item_price="";
+		});
+		$scope.showme = true;
+	};
+			
+	$scope.deleteData = function($event){
+		$scope.ele = $event.target;
+		if($scope.ele.nodeName === "SPAN"){
+			$scope.itemId = $scope.ele.id;
+			$http.delete('/products/'+$scope.itemId).success(function(response,status){
+				if(status === 200){
+					$("#d1 div.well:has(span[id="+$scope.itemId+"])").remove();
+				}
+			});
+		}
+	};
+			
+});
+/*var app = angular.module('myApp', []);
 		
 app.controller('itemsCtrl', function($scope, $http){
 	$http.get("/products").success(function(response){
@@ -43,5 +108,5 @@ app.controller('postItemCtrl', function($scope,$http){
 		}
 	};
 			
-});
+});*/
 		
