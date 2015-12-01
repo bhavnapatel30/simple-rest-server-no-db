da3ccecdbc1f35f9f8ee3c0b9bb5732585d8658e
Bhavna Patel <bhavna.patel333@gmail.com>
2015-11-30 22:33:18 -0500

    Added services

--- public/Project_AngularJs/js/displayData.js
+++ public/Project_AngularJs/js/displayData.js
@@ -1,112 +1,80 @@
 var app = angular.module('myApp', []);
 
-app.factory('itemFactory', function($http){
-	var items;
-	var factory = {};
-	factory.getItems = function(){
-		$http.get("/products").success(function(response){
-			items = response.data;
+app.service('itemService', function($http, $q){
+	
+	this.getItems = function(){
+		var i = $q.defer();
+		$http.get("/products").then(function(response){
+			i.resolve(response);
 		});
-		return items;
+		return i.promise;		
 	};
-	return factory;
-});
-			
-app.controller('itemsCtrl', function ($scope, itemFactory){
-	//$scope.items=[];
-	$scope.items = itemFactory.getItems();
-	console.log($scope.items);					
+	
+	this.postItem = function(new_item){
+		var i = $q.defer();
+		$http({
+			method:'POST',
+			url:'/products',
+			data:new_item
+		}).then(function(response){
+			i.resolve(response);
+		});
+		return i.promise;
+	};
+	
+	this.deleteItem = function(new_id){
+		var i = $q.defer();
+		$http.delete('/products/'+new_id).then(function(response){
+			i.resolve(response);
+		});
+		return i.promise;
+	}
 });
 
-		
-/*app.controller('itemsCtrl', function($scope, $http){
-	$http.get("/products").success(function(response){
-		$scope.items = response;
-	});
-});
-		
-app.controller('postItemCtrl', function($scope,$http){
+app.controller('itemsCtrl', function($scope, itemService){
 	$scope.newItem = {};
 	$scope.showme = false;
-			
+	
+	var promise = itemService.getItems();
+	promise.then(function(data){
+		$scope.items = data.data;
+	});		
+	
 	$scope.display = function(){
 		$scope.newItem  = {
 			itemName: $scope.item_name,
 			itemDescription: $scope.item_description,
 			itemPrice: $scope.item_price
 		};
-
-		$http({
-			method:'POST',
-			url:'/products',
-			data:$scope.newItem
-		}).success(function(response){
-			$scope.newItem = response;
-
+		
+		// Calling Service
+		var promise = itemService.postItem($scope.newItem);
+		promise.then(function(data){
+			$scope.newItem = data.data;
+			
 			$scope.item_name="";
 			$scope.item_description="";
 			$scope.item_price="";
 		});
 		$scope.showme = true;
 	};
-			
+	
 	$scope.deleteData = function($event){
 		$scope.ele = $event.target;
 		if($scope.ele.nodeName === "SPAN"){
 			$scope.itemId = $scope.ele.id;
-			$http.delete('/products/'+$scope.itemId).success(function(response,status){
-				if(status === 200){
+			
+			// Calling Service
+			var promise = itemService.deleteItem($scope.itemId);
+			promise.then(function(data){
+				if(data.status === 200){
 					$("#d1 div.well:has(span[id="+$scope.itemId+"])").remove();
 				}
 			});
 		}
 	};
-			
+	
 });
-/*var app = angular.module('myApp', []);
-		
-app.controller('itemsCtrl', function($scope, $http){
-	$http.get("/products").success(function(response){
-		$scope.items = response;
-	});
-});
-		
-app.controller('postItemCtrl', function($scope,$http){
-	$scope.newItem = {};
-	$scope.showme = false;
-			
-	$scope.display = function(){
-		$scope.newItem  = {
-			itemName: $scope.item_name,
-			itemDescription: $scope.item_description,
-			itemPrice: $scope.item_price
-		};
 
-		$http({
-			method:'POST',
-			url:'/products',
-			data:$scope.newItem
-		}).success(function(response){
-			$scope.newItem = response;
 
-			$scope.item_name="";
-			$scope.item_description="";
-			$scope.item_price="";
-		});
-		$scope.showme = true;
-	};
-			
-	$scope.deleteData = function($event){
-		$scope.ele = $event.target;
-		if($scope.ele.nodeName === "SPAN"){
-			$scope.itemId = $scope.ele.id;
-			$http.delete('/products/'+$scope.itemId).success(function(response,status){
-				if(status === 200){
-					$("#d1 div.well:has(span[id="+$scope.itemId+"])").remove();
-				}
-			});
-		}
-	};
-			
-});*/
 		
